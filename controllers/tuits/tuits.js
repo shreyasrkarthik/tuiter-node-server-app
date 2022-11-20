be6206be8ebe212a026a526a8eb34110e32df02f
shreyasrkarthik <shreyas.rkarthik@gmail.com>
2022-11-20 18:56:48 -0500

    Trying to fix heroku error

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -5,7 +5,8 @@ export default [
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
         "liked": true, "likes": 2345,
-        "replies": 123, "retuits": 432
+        "disliked": true, "dislikes": 123,
+        "comments": 123, "retuits": 432
     },
     {
         "_id": 234, "topic": "Traffic", "username": "The Boring Company",
@@ -13,6 +14,7 @@ export default [
         "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
         "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
         "liked": true, "likes": 2345,
-        "replies": 123, "retuits": 432
+        "disliked": false, "dislikes": 12,
+        "comments": 123, "retuits": 432
     },
 ];
